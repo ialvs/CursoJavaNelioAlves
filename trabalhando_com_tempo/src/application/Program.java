9bee44f627d8ef8a9a1de9c535a16c1a92880acd
Ian Alves <ian.santos.alves@hotmail.com>
2022-10-24 21:24:07 -0300

    Até a aula #124

--- trabalhando_com_tempo/src/application/Program.java
+++ trabalhando_com_tempo/src/application/Program.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Program {
 
@@ -42,7 +43,7 @@ public class Program {
 		LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
 		Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 		
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		/*DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
 		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
@@ -57,7 +58,47 @@ public class Program {
 		
 		System.out.println("do6 = " + fmt3.format(d06));
 		System.out.println("do6 = " + fmt5.format(d06));
-		System.out.println("do6 = " + d06.toString());
+		System.out.println("do6 = " + d06.toString());*/
+		/*
+		for (String zoneId : ZoneId.getAvailableZoneIds()) {
+			System.out.println(zoneId);
+		}*/
+		
+		/*LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+		LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+		LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+		LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+		
+		System.out.println("r1 = " + r1);
+		System.out.println("r2 = " + r2);
+		System.out.println("r3 = " + r3);
+		System.out.println("r4 = " + r4);
+		
+		System.out.println("d04 dia - " + d04.getDayOfMonth());
+		System.out.println("d04 dia - " + d04.getMonthValue());
+		System.out.println("d04 dia - " + d04.getYear());
+		
+		System.out.println("d05 hora -" + d05.getHour());
+		System.out.println("d05 hora -" + d05.getMinute());*/
+		
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		
+		System.out.println("pastWeekLocalDate - " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate" + nextWeekLocalDate);
+		
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+		
+		System.out.println("pastWeekLocalDateTime -" + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime -" + nextWeekLocalDateTime);
+		
+		Instant pastWeekInstant = d06.minus(7,ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7,ChronoUnit.DAYS);
+		
+		System.out.println("pastWeekInstant - " + pastWeekInstant);
+		System.out.println("nextWeekInstant - " + nextWeekInstant);
+		
 
 	}
 
