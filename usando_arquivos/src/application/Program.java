42806d24ff6a31b1e3fc72efa0aecf805e8fab11
Ian Alves <ian.santos.alves@hotmail.com>
2022-11-02 21:33:34 -0300

    Até a aula #224

--- usando_arquivos/src/application/Program.java
+++ usando_arquivos/src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
 
@@ -69,6 +71,7 @@ public class Program {
 			System.out.println("Error: " + e.getMessage());
 		} */
 		
+		/*
 		String[] lines = new String[] {
 				"Good morning", "Good afternoon", "Good night"
 		};
@@ -83,10 +86,53 @@ public class Program {
 		
 		} catch(IOException e) {
 			e.printStackTrace();
+		}*/
+		
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("Folders: ");
+		
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
 		
-	
-
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("Files: ");
+		
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		
+		System.out.println("Diretório criado com sucesso " + success);
+		
+		sc.close();*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
+		
+				
+		
+		
+		sc.close();
+		
+		
 	}
 
 }
